8a1f8a75f28ab2e35513f334a0f55685cd4a4565
Akinola <akinolatofunmi04@gmail.com>
2022-10-26 13:23:56 -0700

    Task 4 - Makinga an instance from a dict

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,16 @@ from datetime import datetime
 class BaseModel:
     """ Defines the class BaseModel """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initilizes instance of the class BaseModel """
+
+        t_obj_fmt = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == 'updated_at' or key == 'created_at':
+                    self.__dict__[key] = datetime.strptime(value, t_obj_fmt)
+                elif key is not '__class__':
+                    setattr(self, key, value)
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
